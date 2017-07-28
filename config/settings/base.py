91247f34a5938f1050bc92cd18999525f6f46ba1
Brady Endres <bfendres@gmail.com>
2017-07-27 23:07:37 -0700

    updated staticfiles for production

--- config/settings/base.py
+++ config/settings/base.py
@@ -174,13 +174,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = "/var/www/mibura.com/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
 ]
 
 # Media Files

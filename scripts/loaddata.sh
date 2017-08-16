97ce19483d108dad3a6d5f748108e40d1a46a310
Brady Endres <bfendres@gmail.com>
2017-08-15 17:09:18 -0700

    added dumpdata and loaddate scripts

---
+++ scripts/loaddata.sh
@@ -0,0 +1,4 @@
+python manage.py loaddata support/data/Cloud.json;
+python manage.py loaddata support/data/Plan.json;
+python manage.py loaddata support/data/ProductCategory.json;
+python manage.py loaddata support/data/Product.json;

8aff5f645aa1d32dcb05c80a114bf901e095dd33
Brady Endres <bfendres@gmail.com>
2017-09-18 16:12:19 -0700

    updated cart styling on mobile

--- support/frontend/src/App.vue
+++ support/frontend/src/App.vue
@@ -173,7 +173,7 @@
 			<div class="col-xs-12">
 				<progressbar></progressbar>
 			</div>
-			<div class="col-md-12 col-lg-3" style="padding: 0 5px;">
+			<div class="col-xs-12 col-lg-3" style="padding: 0 5px;">
 				<notification v-for="(notification, index) in getNotifications" :data="notification" :index="index" :key="index"></notification>
 			</div>
 			<div id="support-form" class="col-xs-10 col-xs-offset-1 col-md-8 col-md-offset-0 col-lg-6 col-lg-offset-0">
